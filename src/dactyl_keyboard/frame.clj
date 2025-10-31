91bb05fdb41c92004657f0a48b4aec3531be427f
Sergey Petrunin <halfstrik@gmail.com>
2025-10-30 23:49:18 -0400

    Improve base dimensions

--- src/dactyl_keyboard/frame.clj
+++ src/dactyl_keyboard/frame.clj
@@ -4,11 +4,11 @@
             [scad-clj.model :refer :all]))
 
 (def main-outline
-  (let [main-sphere (->> (with-fn 200 (sphere 900))
-                         (translate [0 5 -850]))
-        main-cube-length 400
-        main-cube-width 200
-        main-cube-heigh 50
+  (let [main-sphere (->> (with-fn 300 (sphere 1400))
+                         (translate [0 -110 -1326]))
+        main-cube-length 420
+        main-cube-width 186
+        main-cube-heigh 70
         main-cube (->> (cube main-cube-length main-cube-width main-cube-heigh)
                        (translate [0 (/ main-cube-width 2) (/ main-cube-heigh 2)]))]
     (intersection main-sphere main-cube)
