5ecb0941e31831c0498982777158786caff2eba4
Sergey Petrunin <halfstrik@gmail.com>
2025-10-31 22:45:48 -0400

    Draft, added right sphere for a well, still need adjustment

--- src/dactyl_keyboard/frame.clj
+++ src/dactyl_keyboard/frame.clj
@@ -3,6 +3,9 @@
   (:require [scad-clj.scad :refer :all]
             [scad-clj.model :refer :all]))
 
+(defn write-scad-gen4 [shape]
+  (write-scad (translate [0 58 0] (mirror [0 1 0] shape))))
+
 (def main-outline
   (let [main-sphere (->> (with-fn 300 (sphere 1400))
                          (translate [0 -110 -1326]))
@@ -16,3 +19,13 @@
 
 (spit "things_frame/base.scad"
       (write-scad main-outline))
+
+(def well-sphere
+  (let [sphere (->> (with-fn 150 (sphere 82))
+                    (translate [103 57 94])
+                    (scale [1.75 1 1.3]))]
+    (difference main-outline sphere)
+    )) ; TODO: need to align with keys/caps
+
+(spit "things_frame/base_well.scad"
+      (write-scad-gen4 well-sphere))
