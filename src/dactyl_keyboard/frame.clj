89ee5437abab120b3896ed002e87517950d05205
Sergey Petrunin <halfstrik@gmail.com>
2025-11-02 00:39:54 -0400

    Added inner cut to form top plate walls and define thickness

--- src/dactyl_keyboard/frame.clj
+++ src/dactyl_keyboard/frame.clj
@@ -17,14 +17,25 @@
     (intersection main-sphere main-cube)
     ))
 
+(def main-inline
+  (let [main-sphere (->> (with-fn 300 (sphere 1400))
+                          (translate [0 -110 (+ -1326 -2)]))
+         main-cube-length (- 420 4)
+         main-cube-width (- 186 4)
+         main-cube-heigh 70
+         main-cube (->> (cube main-cube-length main-cube-width main-cube-heigh)
+                        (translate [0 (+ (/ main-cube-width 2) 2) (- (/ main-cube-heigh 2) 2)]))]
+     (intersection main-sphere main-cube)
+     ))
+
 (spit "things_frame/base.scad"
-      (write-scad main-outline))
+      (write-scad (difference main-outline main-inline)))
 
 (def well-sphere
   (let [sphere (->> (with-fn 150 (sphere 82))
                     (translate [103 57 94])
                     (scale [1.75 1 1.3]))]
-    (difference main-outline sphere)
+    (difference (difference main-outline main-inline) sphere)
     )) ; TODO: need to align with keys/caps
 
 (spit "things_frame/base_well.scad"
