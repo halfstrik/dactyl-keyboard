e2b9b34654d6687977935fb4d1e2fcbe4b623415
Sergey Petrunin <halfstrik@gmail.com>
2026-03-30 23:11:43 -0400

    Improve micro usb plug

--- src/dactyl_keyboard/frame.clj
+++ src/dactyl_keyboard/frame.clj
@@ -893,15 +893,27 @@
             ))
         ))
 
-(def micro-usb-female-socket
-  (->> (polygon [[-4.0 -1.5] [4.0 -1.5] [3.0 1.0] [-3.0 1.0]]) ; Define the 2D trapezoid points
-       (extrude-linear {:height 20})
+(def micro-usb-male-socket
+  (->> (minkowski
+         (->> (polygon [[-3.55 -1.05] [3.55 -1.05] [2.6 1.05] [-2.6 1.05]])
+              (extrude-linear {:height 20}))
+         ;; The "rounding" tool
+         (with-fn 25 (cylinder 0.3 0.1)))
        (rotate (/ π 2) [-1 0 0])
-       (translate [0 35 25])))     ; Extrude it vertically
+       (translate [0 23 25])))
 
+(def micro-usb-female-socket
+  (->> (minkowski
+         (->> (polygon [[-3.95 -1.25] [3.95 -1.25] [2.9 1.05] [-2.9 1.05]])
+              (extrude-linear {:height 20}))
+         ;; The "rounding" tool
+         (with-fn 25 (cylinder 0.3 0.1)))
+       (rotate (/ π 2) [-1 0 0])
+       (translate [0 42.2 25])))
 
 (spit "things_frame/micro_usb_plug.scad"
       (write-scad
+        ;micro-usb-female-socket
         (difference
           (->> (cube 28 2.6 10)
                (translate [0 33 25]))
@@ -915,6 +927,7 @@
                    (translate [0 31.5 25]))
               ; back plate
               usb-hole-cut)
+            micro-usb-male-socket
             micro-usb-female-socket
             ))
         ))
